da5a1d5592a71caa7e1dff297566203ae10c90da
Saša Barišić <sasa.barisic@serengetitech.com>
2022-04-04 19:56:14 +0200

    latest

--- GMLAN/main/main.ino
+++ GMLAN/main/main.ino
@@ -7,9 +7,11 @@ MCP_CAN CAN0(10);      // CS pin
 #define CAN1_INT 2
 MCP_CAN CAN1(9);
 
+typedef long unsigned int rxid_t;
+
 byte byteBuf[256];
 byte byteLen;
-unsigned long rxID;
+rxid_t rxID;
 
 void setup() {
 	Serial.begin(2000000);
@@ -63,35 +65,22 @@ void setup() {
 	CAN1.sendMsgBuf(0xAABB, 1, byteLen, byteBuf);*/
 }
 
-void serial_send(byte CANsrc, unsigned long rxID, byte CAN_len, byte* CAN_buf) {
+void serial_send(byte CANsrc, rxid_t rxID, byte CAN_len, byte* CAN_buf) {
 	Serial.write(CANsrc);
-	Serial.write((byte*)&rxID, sizeof(unsigned long));
+	Serial.write((byte*)&rxID, sizeof(rxid_t));
 	Serial.write(CAN_len);
 	Serial.write(CAN_buf, CAN_len);
 }
 
 
 void loop() {
-    //CAN1.readMsgBuf(&rxID, &byteLen, byteBuf);
-    //serial_send(1, rxID, byteLen, byteBuf);
-  
-	/*if (!digitalRead(CAN0_INT)) {
+	if (!digitalRead(CAN0_INT)) {
 		CAN0.readMsgBuf(&rxID, &byteLen, byteBuf);
 		serial_send(0, rxID, byteLen, byteBuf);
-	}*/
+	}
 	
 	if (!digitalRead(CAN1_INT)) {
 		CAN1.readMsgBuf(&rxID, &byteLen, byteBuf);
 		serial_send(1, rxID, byteLen, byteBuf);
 	}
-	
-	// CAN0.readMsgBuf(&rxID, &byteLen, byteBuf);
-	
-	/*rxID = 0xAABBCCDD;
-	byteLen = 3;
-	byteBuf[0] = 0x45;	
-	byteBuf[1] = 0xAB;	
-	byteBuf[2] = 0x69;
-	
-	serial_send(0, rxID, byteLen, byteBuf);*/
 }
