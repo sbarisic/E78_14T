2bee2811fb49a6dbef5dfda1fb731536684ca41d
Saša Barišić <sasa.barisic@serengetitech.com>
2022-06-30 15:24:18 +0200

    Added virtual dyno

---
+++ VirtualDyno/MainForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1 {
+	public partial class MainForm : Form {
+		List<CustomSeries> AllSeries = new List<CustomSeries>();
+		int MinRPM;
+		int MaxRPM;
+
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		void ClearSeries() {
+			AllSeries.Clear();
+			chart1.Series.Clear();
+		}
+
+		void LoadGraph(DynoDataPoint[] DynoPoints, string Name, Color ColorA, Color ColorB, Color ColorC) {
+			foreach (DynoDataPoint Pt in DynoPoints) {
+				if (Pt.RPM < MinRPM)
+					MinRPM = Pt.RPM;
+
+				if (Pt.RPM > MaxRPM)
+					MaxRPM = Pt.RPM;
+			}
+
+			CustomSeries PowerSeries = CreateSeries(chart1, Name + " HP", ColorA, SeriesType.RPM);
+			CustomSeries TorqueSeries = CreateSeries(chart1, Name + " Nm", ColorB, SeriesType.RPM);
+			CustomSeries BoostSeries = CreateSeries(chart1, Name + " Boost x 100", ColorC, SeriesType.RPM);
+
+			for (int i = 0; i < DynoPoints.Length; i++) {
+				PowerSeries.Series.Points.AddXY(DynoPoints[i].RPM, DynoPoints[i].HP);
+				TorqueSeries.Series.Points.AddXY(DynoPoints[i].RPM, DynoPoints[i].Nm);
+
+				BoostSeries.Series.Points.AddXY(DynoPoints[i].RPM, DynoPoints[i].Boost * 100);
+			}
+
+			EnableSeries(SeriesType.RPM);
+		}
+
+		CustomSeries CreateSeries(Chart chrt, string Name, Color Clr, SeriesType SType) {
+			Series series = chrt.Series.Add(Name);
+			series.ChartType = SeriesChartType.Line; //SeriesChartType.Spline;
+			series.Color = Clr;
+			series.BorderWidth = 1;
+			series.MarkerStyle = MarkerStyle.Circle;
+			//series.SetCustomProperty("LineTension", "0.1");
+
+			CustomSeries S = new CustomSeries(chrt, series, SType);
+			AllSeries.Add(S);
+
+			return S;
+		}
+
+		void EnableSeries(SeriesType SType) {
+			foreach (CustomSeries S in AllSeries) {
+				S.Series.Enabled = S.SeriesType == SType;
+			}
+
+			double XMin = 0;
+			double XMax = 0;
+			double XInterval = 0;
+
+			switch (SType) {
+				/*case SeriesType.Time:
+					XMin = MinTime;
+					XMax = MaxTime;
+					XInterval = 1;
+					break;*/
+
+				case SeriesType.RPM:
+					XMin = MinRPM;
+					XMax = MaxRPM;
+					XInterval = 500;
+
+					break;
+
+				default:
+					throw new NotImplementedException();
+			}
+
+			SetChart(chart1, XMin, XMax, XInterval, 10);
+		}
+
+		void SetChart(Chart Chrt, double XMin, double XMax, double XInterval, double YInterval) {
+			Chrt.ChartAreas[0].AxisX.Minimum = XMin;
+			Chrt.ChartAreas[0].AxisX.Maximum = XMax;
+			Chrt.ChartAreas[0].AxisX.Interval = XInterval;
+			Chrt.ChartAreas[0].AxisY.Interval = YInterval;
+			Chrt.ResetAutoValues();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e) {
+			MinRPM = 99999;
+			MaxRPM = 0;
+
+
+			LoadGraph(Program.ParseEntriesHPT("dynorun.csv"), "dynorun", Color.Red, Color.Blue, Color.DarkCyan);
+
+			/*DynoDataPoint[] Hypothetical = Program.ParseEntriesHPT("corsa2019_run1.csv");
+			Program.TamperNM(Hypothetical, 4500, 260);
+
+			LoadGraph(Hypothetical, "Hyp", Color.FromArgb(200, 50, 60), Color.FromArgb(50, 160, 200), Color.DarkCyan);*/
+		}
+	}
+
+	class CustomSeries {
+		public Chart Chart;
+		public Series Series;
+		public SeriesType SeriesType;
+
+		public CustomSeries(Chart Chart, Series Series, SeriesType SeriesType) {
+			this.Chart = Chart;
+			this.Series = Series;
+			this.SeriesType = SeriesType;
+		}
+	}
+
+	enum SeriesType {
+		Time,
+		RPM
+	}
+}
---
+++ VirtualDyno/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace WindowsFormsApp1 {
+	internal static class Program {
+		[STAThread]
+		static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+
+		public static void TamperNM(DynoDataPoint[] Points, int FromRPM, int NewNM) {
+			for (int i = 0; i < Points.Length; i++) {
+				if (Points[i].RPM >= FromRPM) {
+					Points[i].Nm = NewNM;
+				}
+			}
+		}
+
+		public static DynoDataPoint[] ParseEntriesHPT(string CSVFile) {
+			string[] Lines = File.ReadAllText(CSVFile).Trim().Split(new[] { '\n' }).Select(L => L.Trim()).ToArray();
+
+			int ChannelInfoIdx = FindIndex(Lines, "[Channel Information]");
+			int ChannelDataIdx = FindIndex(Lines, "[Channel Data]");
+
+			string[] ChannelInfo = Lines.Skip(ChannelInfoIdx + 1).Take(ChannelDataIdx - ChannelInfoIdx - 1).ToArray();
+			string[] InfoNames = ChannelInfo[1].Split(new[] { ',' }).ToArray();
+
+			int RPMIdx = -1;
+			int AirflowIdx = -1;
+			int BoostIdx = -1;
+			int BaroIdx = -1;
+
+			for (int i = 0; i < InfoNames.Length; i++) {
+				if (InfoNames[i].Contains("Engine RPM"))
+					RPMIdx = i;
+
+				if (InfoNames[i].Contains("Mass Airflow (SAE)"))
+					AirflowIdx = i;
+
+				if (InfoNames[i].Contains("Boost Pressure"))
+					BoostIdx = i;
+
+				if (InfoNames[i].Contains("Barometric"))
+					BaroIdx = i;
+			}
+
+			List<DynoDataPoint> DynoPointsList = new List<DynoDataPoint>();
+
+			string[][] DataLines = Lines.Skip(ChannelDataIdx + 1).Select(L => L.Split(',')).ToArray();
+			for (int i = 0; i < DataLines.Length; i++) {
+				DynoDataPoint Point = new DynoDataPoint(DataLines[i][RPMIdx], DataLines[i][AirflowIdx], DataLines[i][BoostIdx], DataLines[i][BaroIdx]);
+				bool Appended = false;
+
+				foreach (DynoDataPoint ExPoint in DynoPointsList) {
+					if (ExPoint.RPM == Point.RPM) {
+						ExPoint.AppendAverage(Point);
+						Appended = true;
+						break;
+					}
+				}
+
+				if (!Appended) {
+					DynoPointsList.Add(Point);
+				}
+			}
+
+			return DynoPointsList.OrderBy(P => P.RPM).ToArray();
+		}
+
+		static int FindIndex(string[] Lines, string Src) {
+			for (int i = 0; i < Lines.Length; i++) {
+				if (Lines[i] == Src)
+					return i;
+			}
+
+			return 0;
+		}
+	}
+
+	class DynoDataPoint {
+		const float SomeConstant = 0.76f;
+
+		public int RPM;
+		public float Airflow;
+		public float Boost;
+		public float Baro;
+
+		public float Nm {
+			get {
+				return (float)Math.Round(((CalcHP * 5252) / RPM) * 1.35581795f);
+			}
+
+			set {
+				float FTLBS = value / 1.35581795f;
+				float A = FTLBS * RPM;
+				float NewHP = A / 5252;
+				Airflow = NewHP * SomeConstant;
+			}
+		}
+
+		public float HP {
+			get {
+				return (float)Math.Round(CalcHP);
+			}
+		}
+
+		float CalcHP {
+			get {
+				return Airflow / SomeConstant;
+			}
+		}
+
+		public DynoDataPoint(string RPM, string Airflow, string Boost, string Baro) {
+			this.RPM = (int)double.Parse(RPM, CultureInfo.InvariantCulture);
+
+			this.Airflow = (float)double.Parse(Airflow, CultureInfo.InvariantCulture);
+			this.Baro = (float)(double.Parse(Baro, CultureInfo.InvariantCulture) / 100);
+			this.Boost = (float)Math.Round((double.Parse(Boost, CultureInfo.InvariantCulture) / 100) - this.Baro, 2);
+		}
+
+		public void AppendAverage(DynoDataPoint Other) {
+			Airflow = (Airflow + Other.Airflow) / 2;
+			Boost = (Boost + Other.Boost) / 2;
+			Baro = (Baro + Other.Baro) / 2;
+		}
+
+		public override string ToString() {
+			return string.Format("{0} - {1} g/s, {2} bar; {3} Hp, {4} Nm", RPM, Airflow, Boost, HP, Nm);
+		}
+	}
+}
