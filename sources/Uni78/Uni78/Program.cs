1248696441084967e313759715aa7592c60f85e6
Saša Barišić <sasa.barisic@serengetitech.com>
2023-05-09 14:47:43 +0200

    Unicorn

--- sources/Uni78/Uni78/Program.cs
+++ sources/Uni78/Uni78/Program.cs
@@ -1,7 +1,32 @@
-﻿namespace Uni78 {
-	internal class Program {
+﻿using UnicornEngine;
+using UnicornEngine.Binding;
+using UnicornEngine.Const;
+using System.IO;
+using System;
+
+namespace Uni78 {
+	static class Program {
+		static byte[] Firmware;
+
 		static void Main(string[] args) {
 			Console.WriteLine("Hello, World!");
+			Firmware = File.ReadAllBytes("data/firm.bin");
+
+			uint StartAddr = 0x20000; 
+			uint EndAddr = 0x300000;
+
+			Unicorn UC = new Unicorn(Common.UC_ARCH_PPC, Common.UC_MODE_PPC32 | Common.UC_MODE_BIG_ENDIAN);
+			UC.MemMap(0, EndAddr, Common.UC_PROT_ALL);
+			UC.MemWrite(0, Firmware);
+
+			UC.AddCodeHook((uc, address, size, user_data) => {
+				Console.WriteLine("CODE 0x{0:X} - {1}", address, size);
+			}, null, 0, EndAddr);
+
+			UC.EmuStart(StartAddr, 0, 0, 0);
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
 		}
 	}
 }
