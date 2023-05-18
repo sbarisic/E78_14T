cd30d8897e40fa09e7c5a54c933d4ac342419f15
Saša Barišić <sasa.barisic@serengetitech.com>
2023-05-18 14:21:33 +0200

    FirmwareScanner source

---
+++ sources/Uni78/FirmwareScanner/RangeScanner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmwareScanner {
+	class RangeScanner {
+
+		public RangeScanner(string RangeMinInclusive, string RangeMaxInclusive) {
+
+		}
+
+		public void SetData(string Data) {
+
+		}
+
+		public void FindNext() {
+
+		}
+	}
+}
