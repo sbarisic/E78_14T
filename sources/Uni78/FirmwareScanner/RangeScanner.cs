564f07d4f2f8c29212b15e72fc3b8575832f71f5
Saša Barišić <sasa.barisic@serengetitech.com>
2023-05-18 15:53:34 +0200

    Firmware Scanner

--- sources/Uni78/FirmwareScanner/RangeScanner.cs
+++ sources/Uni78/FirmwareScanner/RangeScanner.cs
@@ -3,20 +3,112 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace FirmwareScanner {
 	class RangeScanner {
+		byte[] Bytes;
+		byte[] BytesToScan;
 
-		public RangeScanner(string RangeMinInclusive, string RangeMaxInclusive) {
+		double RangeMinInclusive;
+		double RangeMaxInclusive;
+		double Step;
+		ulong Range;
+		ulong MaxVal;
+		uint Bits;
 
+		public RangeScanner() {
+		}
+
+		public void SetOptions(double RangeMinInclusive, double RangeMaxInclusive, double MinValue, double MaxValue) {
+			this.RangeMinInclusive = RangeMinInclusive;
+			this.RangeMaxInclusive = RangeMaxInclusive;
+			Range = (ulong)(RangeMaxInclusive - RangeMinInclusive);
+			Step = RangeMaxInclusive - MaxValue;
+			MaxVal = (uint)(Range / Step);
+			Bits = (uint)(Math.Log(MaxVal) / Math.Log(2));
+
+			// ConvertToBytes(0, out byte[] ValBytes1);
+			// ConvertToBytes(255.9921875f, out byte[] ValBytes2);
+			// ConvertToBytes(-256, out byte[] ValBytes3);
+		}
+
+		byte[] ConvertToBytes(double Val) {
+			double UVal = Val - RangeMinInclusive;
+			uint UIntVal = (uint)(UVal / Range * MaxVal);
+			double CheckNumber = (UIntVal * Step) + RangeMinInclusive;
+
+			byte[] Ret = null;
+
+			if (Bits == 8) {
+				Ret = new byte[] { (byte)UIntVal };
+			} else if (Bits == 16) {
+				Ret = BitConverter.GetBytes((ushort)UIntVal);
+			} else if (Bits == 32) {
+				Ret = BitConverter.GetBytes((uint)UIntVal);
+			} else {
+				throw new NotImplementedException();
+			}
+
+			return Ret.Reverse().ToArray();
+		}
+
+		double ParseNumber(string Num) {
+			return double.Parse(Num, CultureInfo.InvariantCulture);
+		}
+
+		public void SetBytes(byte[] Bytes) {
+			this.Bytes = Bytes;
 		}
 
 		public void SetData(string Data) {
+			string[] StringNumbers = Data.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+			double[] DoubleNumbers = StringNumbers.Select(ParseNumber).ToArray();
 
+			string ExcelString = string.Join("\t", DoubleNumbers.Select(DN => DN.ToString()));
+
+			if (Data != ExcelString) {
+				Console.WriteLine("[WARN] Data does not match");
+			}
+
+			BytesToScan = DoubleNumbers.SelectMany(F => ConvertToBytes(F)).ToArray();
 		}
 
-		public void FindNext() {
+		public void FindAll() {
+			int[] Offsets = PatternAt(0, Bytes, BytesToScan).ToArray();
 
+			for (int i = 0; i < Offsets.Length; i++) {
+				Console.WriteLine("0x{0:X4} ({0}) - MATCH (len {1})", Offsets[i], BytesToScan.Length);
+			}
+		}
+
+		static IEnumerable<int> PatternAt(int Start, byte[] Src, byte[] Pattern) {
+			for (int i = Start; i < Src.Length; i++) {
+				bool Matches = true;
+
+				for (int j = 0; j < Pattern.Length; j++) {
+					int Idx = i + j;
+
+					if (Idx >= Src.Length) {
+						Matches = false;
+						break;
+					}
+
+					if (Src[Idx] != Pattern[j]) {
+						Matches = false;
+						break;
+					}
+				}
+
+				if (Matches) {
+					yield return i;
+				}
+
+
+				/*if (Src.Skip(i).Take(Pattern.Length).SequenceEqual(Pattern)) {
+					yield return i;
+				}*/
+			}
 		}
 	}
 }
---
+++ sources/Uni78/FirmwareScanner/ScannerProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FirmwareScanner {
+	internal class Program {
+		static void Main(string[] args) {
+			byte[] Firm = File.ReadAllBytes("data/firm.bin");
+
+			RangeScanner RS = new RangeScanner();
+			RS.SetBytes(Firm);
+
+			// Short pulse adder
+			//RS.SetOptions(-256, 256, -256, 255.9921875f);
+			//RS.SetData("5.75\t4.375\t3.125\t1.875\t1.125\t0.625\t0.25\t0.125\t0\t-0.125\t-0.125\t-0.25\t-0.25\t-0.25\t-0.25\t-0.25\t-0.25\t-0.25\t-0.25\t-0.25\t-0.125\t-0.125\t-0.125\t-0.125\t-0.125\t-0.125");
+
+			// Stoich AFR
+			RS.SetOptions(0, 32, 0, 31.99951171875f);
+			RS.SetData("14.1298828125\t14.1298828125\t13.9853515625\t13.62744140625\t13.27001953125\t12.91259765625\t12.55517578125\t12.19775390625\t11.84033203125\t11.482421875\t11.125\t10.767578125\t10.41015625\t10.052734375\t9.69482421875\t9.33740234375\t8.97998046875");
+
+
+			RS.FindAll();
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+	}
+}
