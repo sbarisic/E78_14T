045b26aeab9fae4336f8b4d0c2d62585c8788a32
Saša Barišić <sasa.barisic@serengetitech.com>
2023-06-06 13:45:04 +0200

    Damos CSV parser

--- sources/Uni78/DamosCSVParser/Program.cs
+++ sources/Uni78/DamosCSVParser/Program.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Windows.Forms;
 
 namespace DamosCSVParser {
 	internal class Program {
+
+		[STAThread]
 		static void Main(string[] args) {
 			Damos D = new Damos("data/winols_astra.csv");
-
-			File.WriteAllLines("Lines.txt", D.Entries.Select(E => E.ToString()).ToArray());
+			//File.WriteAllLines("Lines.txt", D.Entries.Select(E => E.ToString()).ToArray());
 		}
 	}
 }
