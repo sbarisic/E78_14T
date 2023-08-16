1e2d818d2655818fc454deabd53139a3dad88827
Saša Barišić <sasa.barisic@serengetitech.com>
2023-08-16 08:38:15 +0200

    Refactoring

--- sources/CorsaCAN/src/Arduino_CAN.cpp
+++ sources/CorsaCAN/src/Arduino_CAN.cpp
@@ -1,17 +1,17 @@
 #include <SPI.h>              //Library for using SPI Communication 
-#include <mcp2515.h>          //Library for using CAN Communication (https://github.com/autowp/arduino-mcp2515/)
+//#include <mcp2515.h>          //Library for using CAN Communication (https://github.com/autowp/arduino-mcp2515/)
 
-struct can_frame canMsg;
-MCP2515 mcp2515(10);                 // SPI CS Pin 10
+//struct can_frame canMsg;
+//MCP2515 mcp2515(10);                 // SPI CS Pin 10
  
 void setup()
 {
   Serial.begin(115200);                //Begins Serial Communication at 9600 baudrate
   Serial.println("Running!");
  
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_16MHZ); //Sets CAN at speed 500KBPS and Clock 8MHz
-  mcp2515.setNormalMode();                  //Sets CAN at normal mode
+  //mcp2515.reset();
+  //mcp2515.setBitrate(CAN_500KBPS, MCP_16MHZ); //Sets CAN at speed 500KBPS and Clock 8MHz
+  //mcp2515.setNormalMode();                  //Sets CAN at normal mode
 }
  
 
@@ -19,7 +19,7 @@ char buffer[6];
  
 void loop()
 {
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) // To receive data (Poll Read)
+  /*if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) // To receive data (Poll Read)
   {
     Serial.print("ID: 0x");
     Serial.print(canMsg.can_id, HEX);
@@ -36,5 +36,5 @@ void loop()
     }
 
     Serial.println();
-  }
+  }*/
 }
---
+++ sources/CorsaCore/src/main.c
@@ -0,0 +1,5 @@
+#include <driver/uart.h>
+
+void app_main() {
+
+}
